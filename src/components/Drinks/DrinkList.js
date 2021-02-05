b4a03e539871e35593374f49f5887fbf9bbeca21
Rhowie86 <ronaldnhowie@gmail.com>
2021-02-05 13:14:13 -0600

    cleaning up unused code in drinkList

--- src/components/Drinks/DrinkList.js
+++ src/components/Drinks/DrinkList.js
@@ -1,27 +1,18 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { DrinkContext } from "./DrinkProvider";
 import { DrinkCard } from "./DrinkCard";
-import { GlasswareContext } from "../Glassware/GlasswareProvider";
-import { IngredientContext } from "../Ingredients/IngredientProvider";
-import { MeasurementContext } from "../Measurements/MeasurementProvider";
 import { CategoryContext } from "../Category/CategoryProvider";
 import { UserContext } from "../Users/UserProvider";
 
 export const DrinkList = () => {
   const { drinks, getDrinks } = useContext(DrinkContext);
-  const { measurement, getMeasurements } = useContext(MeasurementContext);
-  const { ingredient, getIngredients } = useContext(IngredientContext);
-  const { glassware, getGlassware } = useContext(GlasswareContext);
   const { category, getCategory } = useContext(CategoryContext);
   const { user, getUsers } = useContext(UserContext);
 
   useEffect(() => {
     getDrinks()
       .then(getCategory)
-      .then(getGlassware)
-      .then(getIngredients)
-      .then(getMeasurements)
       .then(getUsers);
   }, []);
 
